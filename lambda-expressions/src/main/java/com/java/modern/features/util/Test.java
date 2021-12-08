de8a3501a02a70633370c0f8905af4e5033f142c
dasun <dasamelbdev@gmail.com>
2021-12-08 17:36:19 +0530

    adding Test.java for consumer functional interface examples

---
+++ lambda-expressions/src/main/java/com/java/modern/features/util/Test.java
@@ -0,0 +1,15 @@
+package com.java.modern.features.util;
+
+public class Test {
+
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
